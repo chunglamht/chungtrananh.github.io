e0880bfda84c631293fdc0807160b76180fbe362
chunglamht <chunglamht@gmail.com>
2016-11-22 11:59:01 +0700

    added index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+	<head>
+		<title>Hello world</title>
+		<meta charset="utf8">
+		<style type="text/css">
+		.no-copy {
+    -webkit-touch-callout: none;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+		</style>
+	</head>
+	<body>
+		<h1><?php echo "Hello world"; ?></h1>
+	</body>
+</html>
